72d12aba9294cf5d44ed8741bd2ac98cc105d873
naichaozhang <naichaozhang9@gmail.com>
2017-04-14 16:57:39 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1glAXU2FJoaSjo01dA6dIF6pQkKprKmQ7F5mEhDmx62w/edit#gid=0';
